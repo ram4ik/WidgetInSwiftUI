126446dbfed7b290de75bcd389e7760f28692c61
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-23 11:35:33 +0300

    Initial Commit

---
+++ WidgetInSwiftUI/WidgetInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetInSwiftUIApp.swift
+//  WidgetInSwiftUI
+//
+//  Created by ramil on 23.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct WidgetInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
